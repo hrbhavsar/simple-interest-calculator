f87ff8ba4a56856a28d0ee51ed8bbfebe7024f19
hrbhavsar <hrbhavsar@yahoo.com>
2025-08-29 14:33:28 +0400

    Update simple-interest.sh
    Simple Interest calculator

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,10 @@
-
+# Input Principal
+echo "Enter Principal Amount:"
+read principal
+# Input Rate of Interest in decimal- i.e 5%
+echo "Enter Interest Amount in %:"
+read rate
+echo "Enter Duration of time in months:"
+read time
+simpleinterest = ($pricipal*$rate*$time)/(100*12)
+echo $simpleinterest
